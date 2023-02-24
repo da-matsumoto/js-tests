876c182d055a91eaf504597a8c8977132497fe3f
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2023-02-24 11:35:07 +0900

    cli 作業

---
+++ cli_test/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const { argv } = yargs(hideBin(process.argv))
+  .option('name', {
+    describe: 'CLI名を表示'
+  })
+  .option('file', {
+    describe: 'Markdownファイルのパス'
+  });
+
+if (argv.name) {
+  const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8'});
+  const package = JSON.parse(packageStr);
+  console.log(package.name);
+
+  process.exit(0);
+}
+
+const markdownStr = fs.readFileSync(path.resolve(__dirname, argv.file), { encoding: 'utf-8'});
+console.log(markdownStr);
